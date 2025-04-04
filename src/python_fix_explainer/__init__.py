a029920f61d9d5324cd98b72912b210084229936
Yana <kuziavra@gmail.com>
2025-04-04 14:46:45 -0500

    Export gen_annotated_html

--- src/python_fix_explainer/__init__.py
+++ src/python_fix_explainer/__init__.py
@@ -5,9 +5,12 @@ from .interface_funcs import \
 
 from .muast import breadth_first, MutableAst
 
+from .tree_to_html import gen_annotated_html
+
 __all__ = [
     'generate_edit_scripts', 'simplify_and_choose_shortest',
     'generate_fix_sequence', 'get_run_trace', 'fix_code', 'fix_code_doctest_tests', 'generate_correction',
     'has_failing_unit_test', 'test_all',
-    'breadth_first', 'MutableAst'
+    'breadth_first', 'MutableAst',
+    'gen_annotated_html'
 ]
